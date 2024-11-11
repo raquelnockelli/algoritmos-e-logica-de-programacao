4774da47f0110a62e96b9eda306e04789d073540
Raquel Nockelli <r.nockelli@gmail.com>
2024-11-10 22:50:00 -0300

    Update L2E3.c

--- L2E3.c
+++ L2E3.c
@@ -5,17 +5,18 @@ void main (void)
   char Sexo; // Armazena um dado caracter
   float Altura, PesoIdeal;
   
-  printf("Digite o sexo (M-Masculino  F-Feminino): ");
-  scanf("%c&", &Sexo); // Indica a entrada de um caracter
+  printf("Digite o sexo (M-Masculino ou F-Feminino): ");
+  scanf("%c", &Sexo); // Indica a entrada de um caracter
+  
   printf("Digite a altura: ");
-  scanf("%f&", &Altura); 
-  if (Sexo == 'M')
+  scanf("%f", &Altura); 
+ 
+  if (Sexo == 'M' || Sexo == 'm')
      PesoIdeal = (72.7 * Altura) - 58;
-  if (Sexo == 'm')
-     PesoIdeal = (72.7 * Altura) - 58;
-  if (Sexo == 'F')
+     
+  if (Sexo == 'F' || Sexo == 'f')
      PesoIdeal = (62.1 * Altura) - 44.7;  
-  if (Sexo == 'f')
-     PesoIdeal = (62.1 * Altura) - 44.7;  
+  
   printf("Peso ideal: %.1f", PesoIdeal);
+  
 }
